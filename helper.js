b44ca4c57a3a96057040da490c994670781206e2
Maxim <maxtab1@mail.ru>
2023-11-11 19:41:01 +0300

    regular

--- helper.js
+++ helper.js
@@ -3,7 +3,18 @@ log = console.log;
 function getHeaderFlexBasis() {
 	const header = document.querySelector(".header");
 	const styles = window.getComputedStyle(header);
-	const prop = styles.getPropertyValue("flex-basis");
-	log(`flex-basis: ${prop}`);
+	const flexBasis = styles.getPropertyValue("flex-basis");
+	const flexGrow = styles.getPropertyValue("flex-grow");
+	const flexShrink = styles.getPropertyValue("flex-shrink");
+	log(`flex-basis: ${flexBasis}`);
+	log(`flex-grow: ${flexGrow}`);
+	log(`flex-shrink: ${flexShrink}`);
+
+	const outputDiv = document.querySelector(".headerFlexBasis");
+	outputDiv.innerText = `
+	${flexBasis}
+	${flexGrow}
+	${flexShrink}
+	`;
 }
 getHeaderFlexBasis();
