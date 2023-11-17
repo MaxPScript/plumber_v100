2e65cdacc5ed60bcdd5987cb644f2a3ce03e1264
Maxim <maxtab1@mail.ru>
2023-11-18 02:11:17 +0300

    regular

--- js/window.js
+++ js/window.js
@@ -5,14 +5,16 @@ window.addEventListener("load", (e) => {
 	// const styles = window.getComputedStyle(navbar);
 	// const height = styles.getPropertyValue("height");
 	const output = document.querySelector(".windowWidthAndHeight");
-	output.innerText = `window height: ${window.innerHeight}`;
-	output.innerText = `window width: ${window.innerWidth}`;
+	output.innerText = `window height: ${window.innerHeight} 
+    width: ${window.innerWidth}`;
+	// output.innerText = `window width: ${window.innerWidth}`;
 });
 window.addEventListener("resize", (e) => {
 	// const navbar = document.querySelector(".navbar");
 	// const styles = window.getComputedStyle(navbar);
 	// const height = styles.getPropertyValue("height");
 	const output = document.querySelector(".windowWidthAndHeight");
-	output.innerText = `window height: ${window.innerHeight}`;
-	output.innerText = `window width: ${window.innerWidth}`;
+	output.innerText = `window height: ${window.innerHeight} 
+    width: ${window.innerWidth}`;
+	// output.innerText = `window width: ${window.innerWidth}`;
 });
