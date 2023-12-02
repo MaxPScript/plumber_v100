bcdbef2749d0d7d382c8c41a21adcf5d2b7dfa40
Maxim <maxtab1@mail.ru>
2023-12-02 19:36:29 +0300

    regular

--- js/header__grid.js
+++ js/header__grid.js
@@ -2,7 +2,7 @@ log = console.log;
 
 element = document.querySelector(".header__grid");
 log(element);
-
+element2 = document.querySelector(".header__titles");
 // Get the necessary elements
 // const myDiv = document.getElementById('myDiv');
 // const infoSpan = document.getElementById('infoSpan');
@@ -17,8 +17,18 @@ function updateInfoSpan() {
 	span.style.right = "0";
 	let width = Math.floor(boxRect.width);
 	let height = Math.floor(boxRect.height);
+	// ROOT font-size
+	let html = document.querySelector("html");
+	let htmlStyles = window.getComputedStyle(html);
+	let htmlFontSize = htmlStyles.fontSize;
+	// ROOT font-size
+	// H1 font-size
+	let h1 = element2.querySelector("h1");
+	let h1Styles = window.getComputedStyle(h1);
+	let h1FontSize = h1Styles.fontSize;
+	// H1 font-size
 	// span.textContent = "";
-	span.innerText = `Width: ${width}px, Height: ${height}px, Root font-size: ${window.html}`;
+	span.innerText = `Width: ${width}px, Height: ${height}px, Root font-size: ${htmlFontSize}, H1 font-size: ${h1FontSize}`;
 	// element.removeChild(span);
 }
 
