d59b0ae9d15c4a896f36c81dbfa958c3ae18cecd
Maxim <maxtab1@mail.ru>
2023-12-11 23:49:22 +0300

    regular for gh-pages branch

--- js/nav.js
+++ js/nav.js
@@ -16,17 +16,13 @@ window.addEventListener("resize", (e) => {
 });
 
 window.addEventListener("scroll", (e) => {
-	// let scrollY = window.scrollY;
 	let nav = document.querySelector(".navbar");
-	// nav.style.top = `${getWindowScrollY()}px`;
 	if (getWindowScrollY() > 100) {
-		// nav.classList.remove("opacity_1");
-		nav.classList.add("opacity_0");
+		// nav.classList.add("opacity_0");
 	} else {
-		nav.classList.remove("opacity_0");
-		// nav.classList.add("opacity_1");
+		// nav.classList.remove("opacity_0");
 	}
 });
 function getWindowScrollY() {
-	return window.scrollY;
+	// return window.scrollY;
 }
--- js/window.js
+++ js/window.js
@@ -1,20 +1,49 @@
 log = console.log;
+const output = document.querySelector(".windowWidthAndHeight");
 
 window.addEventListener("load", (e) => {
 	// const navbar = document.querySelector(".navbar");
 	// const styles = window.getComputedStyle(navbar);
 	// const height = styles.getPropertyValue("height");
-	const output = document.querySelector(".windowWidthAndHeight");
+	// const output = document.querySelector(".windowWidthAndHeight");
 	output.innerText = `window height: ${window.innerHeight} 
-    window width: ${window.innerWidth}`;
+    window width: ${window.innerWidth}
+	devicePixelRatio: ${window.devicePixelRatio}`;
 	// output.innerText = `window width: ${window.innerWidth}`;
 });
 window.addEventListener("resize", (e) => {
 	// const navbar = document.querySelector(".navbar");
 	// const styles = window.getComputedStyle(navbar);
 	// const height = styles.getPropertyValue("height");
-	const output = document.querySelector(".windowWidthAndHeight");
+	// const output = document.querySelector(".windowWidthAndHeight");
 	output.innerText = `window height: ${window.innerHeight} 
-    window width: ${window.innerWidth}`;
+    window width: ${window.innerWidth}
+	devicePixelRatio: ${window.devicePixelRatio}`;
 	// output.innerText = `window width: ${window.innerWidth}`;
 });
+
+// DPR
+
+let remove = null;
+// const output = document.querySelector("#output");
+
+const updatePixelRatio = () => {
+	if (remove != null) {
+		remove();
+	}
+	const mqString = `(resolution: ${window.devicePixelRatio}dppx)`;
+	const media = matchMedia(mqString);
+	media.addEventListener("change", updatePixelRatio);
+	remove = () => {
+		media.removeEventListener("change", updatePixelRatio);
+	};
+
+	// output.textContent += ` devicePixelRatio: ${window.devicePixelRatio}`;
+	output.innerText = `window height: ${window.innerHeight} 
+    window width: ${window.innerWidth}
+	devicePixelRatio: ${window.devicePixelRatio}`;
+};
+
+updatePixelRatio();
+
+// DPR
