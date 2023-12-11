4357113922d808a8ba4d55de29fb19e70be9978c
Maxim <maxtab1@mail.ru>
2023-12-12 02:31:17 +0300

    regular for gh-pages branch

--- js/window.js
+++ js/window.js
@@ -2,28 +2,31 @@ log = console.log;
 const output = document.querySelector(".windowWidthAndHeight");
 
 window.addEventListener("load", (e) => {
+	const htmlEl = document.querySelector("html");
 	// const navbar = document.querySelector(".navbar");
 	// const styles = window.getComputedStyle(navbar);
 	// const height = styles.getPropertyValue("height");
 	// const output = document.querySelector(".windowWidthAndHeight");
 	output.innerText = `window height: ${window.innerHeight} 
     window width: ${window.innerWidth}
-	devicePixelRatio: ${window.devicePixelRatio.toFixed(2)}`;
+	devicePixelRatio: ${window.devicePixelRatio.toFixed(2)}
+	html offsetWidth: ${htmlEl.offsetWidth}`;
 	// output.innerText = `window width: ${window.innerWidth}`;
 });
 window.addEventListener("resize", (e) => {
+	const htmlEl = document.querySelector("html");
 	// const navbar = document.querySelector(".navbar");
 	// const styles = window.getComputedStyle(navbar);
 	// const height = styles.getPropertyValue("height");
 	// const output = document.querySelector(".windowWidthAndHeight");
 	output.innerText = `window height: ${window.innerHeight} 
     window width: ${window.innerWidth}
-	devicePixelRatio: ${window.devicePixelRatio.toFixed(2)}`;
+	devicePixelRatio: ${window.devicePixelRatio.toFixed(2)}
+	html offsetWidth: ${htmlEl.offsetWidth}`;
 	// output.innerText = `window width: ${window.innerWidth}`;
 });
 
 // DPR
-
 let remove = null;
 // const output = document.querySelector("#output");
 
